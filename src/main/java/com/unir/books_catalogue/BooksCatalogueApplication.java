c02aafdf90b5be0045eb575dd403222d558c104c
Adrián Pulido <adrianpulido@192.168.1.6>
2025-07-03 22:04:03 -0500

    se ajusta main aplication

--- src/main/java/com/unir/books_catalogue/BooksCatalogueApplication.java
+++ src/main/java/com/unir/books_catalogue/BooksCatalogueApplication.java
@@ -1,5 +1,6 @@
 package com.unir.books_catalogue;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@Slf4j
 public class BooksCatalogueApplication {
 
 	@LoadBalanced
@@ -17,6 +19,16 @@ public class BooksCatalogueApplication {
 	}
 
 	public static void main(String[] args) {
+		// Retrieve execution profile from environment variable. If not present, default profile is selected.
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
+		// Railway's internal interface takes some time to start. We wait for it to be ready.
+		log.debug("Waiting fo Internal Interface to start...");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 		SpringApplication.run(BooksCatalogueApplication.class, args);
 	}
 }
